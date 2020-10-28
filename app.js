d2e9ff71f84e8057b402a069dee413230987397b
Aban Alanizi <aban3654@gmail.com>
2020-10-28 20:37:43 +0300

    First Addition

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+  var typed = new Typed('#animated-text', {
+    strings: [
+      'Full Stack Developer',
+      'Web Designer',
+      'WordPress Developer',
+      'And Much More.'
+    ],
+    typeSpeed: 70,
+    backSpeed: 70,
+    
+  })
+})
